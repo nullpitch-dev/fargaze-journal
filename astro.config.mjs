7d103cdf773fc8857069ee013df5d000840b2375
Hyoje <hyoje.choi@ymail.com>
2026-06-03 11:32:18 +0100

    feat: changed domain name for search feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://journal.fargaze.co',
   integrations: [mdx(), sitemap()],
 
   fonts: [
